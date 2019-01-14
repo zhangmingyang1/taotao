bba6c30189debf2f9c9b2956b93e26b42a243d30
zhangmingyang1 <291974786@qq.com>
2019-01-14 22:54:06 +0800

    淘淘商城v1.2

---
+++ taotao-foreground/src/main/java/com/zte/km/dto/UserMessageImg.java
@@ -0,0 +1,33 @@
+package com.zte.km.dto;
+
+public class UserMessageImg {
+    private Long id;
+
+    private Long userId;
+
+    private String picture;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
